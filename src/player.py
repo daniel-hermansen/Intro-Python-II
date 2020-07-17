bb41230ee86b7fc7713ce51592d4948d577b4dd6
Daniel Hermansen <daniel.j.hermansen@gmail.com>
2020-07-16 19:23:50 -0600

    Finished day 1 MVP

--- src/player.py
+++ src/player.py
@@ -1,2 +1,17 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, name, room):
+        self.name = name
+        self.room = room
+        self.inventory = []
+
+    def __str__(self):
+        return f"{self.name} is in {self.room}"
+
+    def pickup_item(self, item):
+        self.inventory.append(item)
+
+    def drop_item(self,item):
+        self.inventory.remove(item)
